7528ddf99c9e593075f6c7234e3341531fd362c0
CesarLoor <ciloor2@espe.edu.ec>
2025-07-07 15:00:45 -0500

    servicios completos

--- Eureka-Server/src/main/java/espe/edu/ec/eureka_server/EurekaServerApplication.java
+++ Eureka-Server/src/main/java/espe/edu/ec/eureka_server/EurekaServerApplication.java
@@ -2,12 +2,12 @@ package espe.edu.ec.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // Habilita el servidor de descubrimiento
 public class EurekaServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
--- Eureka-Server/src/test/java/espe/edu/ec/eureka_server/EurekaServerApplicationTests.java
+++ Eureka-Server/src/test/java/espe/edu/ec/eureka_server/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package espe.edu.ec.Ereka_Server;
+package espe.edu.ec.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ErekaServerApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
